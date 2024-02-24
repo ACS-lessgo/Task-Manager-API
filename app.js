5ed9fbbabcc26cb8b400b4bcf497b8ff54a0dbfe
ACS-lessgo <royalcs379@gmail.com>
2024-02-24 14:18:29 +0530

    resolves some errors

--- app.js
+++ app.js
@@ -2,24 +2,23 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 require('dotenv').config()
 
 // middleware
 app.use(express.json());
 app.use(express.static('./public'));
+// app.use(notFound)
+app.use(errorHandlerMiddleware);
 
 // routes
-app.get("/check", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
 
 // port --> 3000
 const port = 3000;
 
 // db
-
 const start = async() => {
   try{
     await connectDB(process.env.MONGO_URI);
